78656988825ec34c3a5ee4269d942d85e8cadd62
FranciscoJoséMelladoOrtiz <franjomor@gmail.com>
2025-05-28 12:32:36 +0200

    style: footer position

--- src/components/images/image-container.tsx
+++ src/components/images/image-container.tsx
@@ -6,7 +6,7 @@ interface ImageContainerProps {
 
 const ImageContainer = ({children}: ImageContainerProps) => {
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 p-8 overflow-y-scroll gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 p-8 gap-4">
             {children}
         </div>
     )
